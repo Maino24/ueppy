643ff575738f2c4f07321f4081e5eac4466551b4
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-05-04 16:21:19 +0200

    css

--- index.php
+++ index.php
@@ -6,17 +6,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style type="text/css">
+    body{
+        background: rgb(85,85,85); 
+        display: flex;
+        margin: auto;
+        justify-content: center;
+        padding-top: 20px
+        }
+     .background {   
+        background: white;
+        
+        padding: 1% 6%;
+        border: 2px solid black;
+        border-radius: 10px;
+    }
+    h1{
+        border-bottom: 2px solid black;
+    }
+    .richiesta{
+        padding-bottom: 10px;
+        font-size: 1.2rem;
+    }
+    #backgroundInput{
+        background: rgb(85,85,85);
+        height: 30px;
+        width: 250px;
+        border-radius: 10px;
+    }
+    #formato{
+        color: grey;
+        font-size: 0.9rem;
+    }
+    </style>
 </head>
 <body>
+    <div class="background">
 
-    <h1>Form di registrazione</h1>
+        <h1>Form di registrazione</h1>
 
-    <form method="post" action="">
-        <label for="">Email:</label>
-        <input type="email" name="email" required>
-        <input type="submit" value="Invia">
-    </form>
-
+        <form method="post" action="">
+            <div class="richiesta">
+                <label >Inserisci la tua mail:</label>
+            </div>
+            <div class="richiesta">
+                <input id="backgroundInput" type="email" name="email" required>
+                <input type="submit" value="Invia">
+            </div>
+            <div id="formato">formato mail: mar@example.com</div>
+        </form>
+    </div>
 
     <?php
 
